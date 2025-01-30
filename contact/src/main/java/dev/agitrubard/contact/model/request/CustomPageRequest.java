a94b676c0347204f87333417554bcfa2606e1013
agitrubard <demiragitrubar@gmail.com>
2025-01-30 20:24:41 +0300

    #11 | Contact | `CustomPageRequest` Has Been Created

---
+++ contact/src/main/java/dev/agitrubard/contact/model/request/CustomPageRequest.java
@@ -0,0 +1,25 @@
+package dev.agitrubard.contact.model.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
+
+@Setter
+public final class CustomPageRequest {
+
+    @Range(min = 1)
+    @NotNull
+    private int page;
+
+    @Getter
+    @NotNull
+    @Range(min = 1, max = 15)
+    private int size;
+
+
+    public int getPage() {
+        return this.page - 1;
+    }
+
+}
