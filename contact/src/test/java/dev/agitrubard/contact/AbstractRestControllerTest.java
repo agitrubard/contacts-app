58dbb400da0fb803b8c428b602215bc0a638014a
agitrubard <demiragitrubar@gmail.com>
2025-01-31 18:17:55 +0300

    #11 | Contact | Test Configurations Have Been Fixed

--- contact/src/test/java/dev/agitrubard/contact/AbstractRestControllerTest.java
+++ contact/src/test/java/dev/agitrubard/contact/AbstractRestControllerTest.java
@@ -5,10 +5,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles({"default", "test"})
 public abstract class AbstractRestControllerTest {
 
     @Autowired
