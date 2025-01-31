8c5b9f01e6ece65412826eb6a65fa073d3249fd9
agitrubard <demiragitrubar@gmail.com>
2025-01-31 22:58:17 +0300

    #7 | Contact | `PersonCreateRequestBuilder` Has Been Created

---
+++ contact/src/test/java/dev/agitrubard/contact/model/request/PersonCreateRequestBuilder.java
@@ -0,0 +1,11 @@
+package dev.agitrubard.contact.model.request;
+
+import dev.agitrubard.contact.model.TestDataBuilder;
+
+public class PersonCreateRequestBuilder extends TestDataBuilder<PersonCreateRequest> {
+
+    public PersonCreateRequestBuilder() {
+        super(PersonCreateRequest.class);
+    }
+
+}
