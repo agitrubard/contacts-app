98c365c45115bf1d6c46fb274e6f6e972f1e979f
agitrubard <demiragitrubar@gmail.com>
2025-01-31 22:46:40 +0300

    #7 | Contact | `PersonCreateRequest` Has Been Created

---
+++ contact/src/main/java/dev/agitrubard/contact/model/request/PersonCreateRequest.java
@@ -0,0 +1,24 @@
+package dev.agitrubard.contact.model.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public final class PersonCreateRequest {
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String lastName;
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String company;
+
+}
