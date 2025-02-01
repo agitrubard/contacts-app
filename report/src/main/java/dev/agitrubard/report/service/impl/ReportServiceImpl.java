4ec5c32f368cf8ee1a5847a1e5e08d9a0dea8bb1
agitrubard <demiragitrubar@gmail.com>
2025-02-01 16:27:28 +0300

    #33 | Report | `findById()` Method Has Been Implemented in `ReportServiceImpl`

--- report/src/main/java/dev/agitrubard/report/service/impl/ReportServiceImpl.java
+++ report/src/main/java/dev/agitrubard/report/service/impl/ReportServiceImpl.java
@@ -1,5 +1,6 @@
 package dev.agitrubard.report.service.impl;
 
+import dev.agitrubard.report.exception.ReportNotFoundException;
 import dev.agitrubard.report.model.Report;
 import dev.agitrubard.report.port.ReportReadPort;
 import dev.agitrubard.report.service.ReportService;
@@ -7,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -14,9 +16,17 @@ class ReportServiceImpl implements ReportService {
 
     private final ReportReadPort reportReadPort;
 
+
     @Override
     public List<Report> findAll(Integer page, Integer pageSize) {
         return reportReadPort.findAll(page, pageSize);
+    }
+
+
+    @Override
+    public Report findById(UUID id) {
+        return reportReadPort.findById(id)
+                .orElseThrow(() -> new ReportNotFoundException(id));
     }
 
 }
