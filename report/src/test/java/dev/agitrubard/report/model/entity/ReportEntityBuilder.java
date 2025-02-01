423f1790f7b2ea79f2980b75e252eb3be8ba627d
agitrubard <demiragitrubar@gmail.com>
2025-02-01 16:33:44 +0300

    #33 | Report | `ReportAdapter.findById()` Method Has Been Covered with Unit Tests

--- report/src/test/java/dev/agitrubard/report/model/entity/ReportEntityBuilder.java
+++ report/src/test/java/dev/agitrubard/report/model/entity/ReportEntityBuilder.java
@@ -3,6 +3,7 @@ package dev.agitrubard.report.model.entity;
 import dev.agitrubard.report.model.TestDataBuilder;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class ReportEntityBuilder extends TestDataBuilder<ReportEntity> {
 
@@ -15,6 +16,11 @@ public class ReportEntityBuilder extends TestDataBuilder<ReportEntity> {
         return this
                 .withCreatedAt(LocalDateTime.now())
                 .withUpdatedAt(LocalDateTime.now().plusMinutes(1));
+    }
+
+    public ReportEntityBuilder withId(UUID id) {
+        data.setId(id);
+        return this;
     }
 
     public ReportEntityBuilder withCreatedAt(LocalDateTime createdAt) {
