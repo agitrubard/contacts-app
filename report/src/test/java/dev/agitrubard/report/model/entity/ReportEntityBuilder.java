3b478091611124531f8809655b212dfd6a09b0cb
agitrubard <demiragitrubar@gmail.com>
2025-02-01 16:03:22 +0300

    #32 | Report | `ReportEntityBuilder` Has Been Created

---
+++ report/src/test/java/dev/agitrubard/report/model/entity/ReportEntityBuilder.java
@@ -0,0 +1,30 @@
+package dev.agitrubard.report.model.entity;
+
+import dev.agitrubard.report.model.TestDataBuilder;
+
+import java.time.LocalDateTime;
+
+public class ReportEntityBuilder extends TestDataBuilder<ReportEntity> {
+
+    public ReportEntityBuilder() {
+        super(ReportEntity.class);
+    }
+
+
+    public ReportEntityBuilder withValidValues() {
+        return this
+                .withCreatedAt(LocalDateTime.now())
+                .withUpdatedAt(LocalDateTime.now().plusMinutes(1));
+    }
+
+    public ReportEntityBuilder withCreatedAt(LocalDateTime createdAt) {
+        data.setCreatedAt(createdAt);
+        return this;
+    }
+
+    public ReportEntityBuilder withUpdatedAt(LocalDateTime updatedAt) {
+        data.setUpdatedAt(updatedAt);
+        return this;
+    }
+
+}
