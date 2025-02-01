e59b6968f2045e0084eca37a8b07f727d8d5437c
agitrubard <demiragitrubar@gmail.com>
2025-02-01 11:00:58 +0300

    #13 | Contact | `PersonContactService` and `PersonContactServiceImpl` Have Been Created and `add()` Method Has Been Implemented

---
+++ contact/src/main/java/dev/agitrubard/contact/service/PersonContactService.java
@@ -0,0 +1,11 @@
+package dev.agitrubard.contact.service;
+
+import dev.agitrubard.contact.model.request.PersonContactAddRequest;
+
+import java.util.UUID;
+
+public interface PersonContactService {
+
+    void add(UUID personId, PersonContactAddRequest addRequest);
+
+}
