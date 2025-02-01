efb0210938baf9ac5ae667d65644b57d7629c6a1
agitrubard <demiragitrubar@gmail.com>
2025-02-01 18:46:38 +0300

    #34 | Report | `create()` Method Has Been Implemented in `ReportServiceImpl` with `ReportCreator`

--- report/src/main/java/dev/agitrubard/report/model/Report.java
+++ report/src/main/java/dev/agitrubard/report/model/Report.java
@@ -2,6 +2,7 @@ package dev.agitrubard.report.model;
 
 import dev.agitrubard.report.model.enums.ReportStatus;
 import dev.agitrubard.report.model.enums.ReportType;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,7 +18,8 @@ public class Report extends AbstractDomainModel {
 
     private UUID id;
     private ReportType type;
-    private ReportStatus status;
+    @Builder.Default
+    private ReportStatus status = ReportStatus.PENDING;
     private String data;
 
 }
--- report/src/main/java/dev/agitrubard/report/service/impl/ReportServiceImpl.java
+++ report/src/main/java/dev/agitrubard/report/service/impl/ReportServiceImpl.java
@@ -1,8 +1,12 @@
 package dev.agitrubard.report.service.impl;
 
 import dev.agitrubard.report.exception.ReportNotFoundException;
+import dev.agitrubard.report.exception.ReportTypeNotImplementedException;
 import dev.agitrubard.report.model.Report;
+import dev.agitrubard.report.model.enums.ReportType;
 import dev.agitrubard.report.port.ReportReadPort;
+import dev.agitrubard.report.port.ReportSavePort;
+import dev.agitrubard.report.service.ReportCreator;
 import dev.agitrubard.report.service.ReportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +19,8 @@ import java.util.UUID;
 class ReportServiceImpl implements ReportService {
 
     private final ReportReadPort reportReadPort;
+    private final ReportSavePort reportSavePort;
+    private final List<ReportCreator> reportCreators;
 
 
     @Override
@@ -27,6 +33,24 @@ class ReportServiceImpl implements ReportService {
     public Report findById(UUID id) {
         return reportReadPort.findById(id)
                 .orElseThrow(() -> new ReportNotFoundException(id));
+    }
+
+
+    @Override
+    public void create(ReportType type) {
+
+        String data = reportCreators.stream()
+                .filter(creator -> type == creator.getType())
+                .findFirst()
+                .orElseThrow(() -> new ReportTypeNotImplementedException(type))
+                .create();
+
+        Report report = Report.builder()
+                .type(type)
+                .data(data)
+                .build();
+
+        reportSavePort.save(report);
     }
 
 }
