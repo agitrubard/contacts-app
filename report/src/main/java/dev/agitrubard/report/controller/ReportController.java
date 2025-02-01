ff18bd1c9599f533fc60d2f48e435bcd61596ef2
agitrubard <demiragitrubar@gmail.com>
2025-02-01 16:30:40 +0300

    #33 | Report | Report Detail Endpoint Has Been Implemented

--- report/src/main/java/dev/agitrubard/report/controller/ReportController.java
+++ report/src/main/java/dev/agitrubard/report/controller/ReportController.java
@@ -2,18 +2,22 @@ package dev.agitrubard.report.controller;
 
 import dev.agitrubard.report.model.Report;
 import dev.agitrubard.report.model.mapper.ReportToListResponseMapper;
+import dev.agitrubard.report.model.mapper.ReportToResponseMapper;
 import dev.agitrubard.report.model.request.CustomPageRequest;
 import dev.agitrubard.report.model.response.CustomSuccessResponse;
 import dev.agitrubard.report.model.response.ReportListResponse;
+import dev.agitrubard.report.model.response.ReportResponse;
 import dev.agitrubard.report.service.ReportService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @Validated
 @RestController
@@ -24,6 +28,7 @@ class ReportController {
     private final ReportService reportService;
 
     private final ReportToListResponseMapper reportToListResponseMapper = ReportToListResponseMapper.INSTANCE;
+    private final ReportToResponseMapper reportToResponseMapper = ReportToResponseMapper.INSTANCE;
 
 
     @GetMapping
@@ -36,6 +41,17 @@ class ReportController {
 
         return CustomSuccessResponse.success(
                 reportToListResponseMapper.map(reports)
+        );
+    }
+
+
+    @GetMapping("/{id}")
+    CustomSuccessResponse<ReportResponse> findById(@PathVariable UUID id) {
+
+        Report report = reportService.findById(id);
+
+        return CustomSuccessResponse.success(
+                reportToResponseMapper.map(report)
         );
     }
 
